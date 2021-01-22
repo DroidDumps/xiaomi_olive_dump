75fa868066d12717f0f13b5c0d3ca012dbcc172c
DroidDumps <rokibhasansagar2014@outlook.com>
2021-01-22 17:21:18 +0000

    Add extras for olive-user 10 QKQ1.191014.001 V12.0.1.0.QCNCNXM release-keys
    Signed-off-by: DroidDumps <rokibhasansagar2014@outlook.com>

---
+++ README.md
@@ -0,0 +1,20 @@
+## olive-user 10 QKQ1.191014.001 V12.0.1.0.QCNCNXM release-keys
+- Manufacturer: xiaomi
+- Platform: msm8937
+- Codename: olive
+- Brand: Xiaomi
+- Flavor: olive-user
+- Release Version: 10
+- Id: QKQ1.191014.001
+- Incremental: V12.0.1.0.QCNCNXM
+- Tags: release-keys
+- CPU Abilist: arm64-v8a,armeabi-v7a,armeabi
+- A/B Device: false
+- Locale: zh-CN
+- Screen Density: 320
+- Fingerprint: Xiaomi/olive/olive:10/QKQ1.191014.001/V12.0.1.0.QCNCNXM:user/release-keys
+- Branch: olive-user-10-QKQ1.191014.001-V12.0.1.0.QCNCNXM-release-keys
+- Repo: xiaomi_olive_dump
+
+
+>Dumped by [Phoenix Firmware Dumper](https://github.com/DroidDumps/phoenix_firmware_dumper)
---
+++ join_split_files.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+cat boot.img.* 2>/dev/null >> boot.img
+rm -f boot.img.* 2>/dev/null
+cat system/system/app/VoiceAssist/VoiceAssist.apk.* 2>/dev/null >> system/system/app/VoiceAssist/VoiceAssist.apk
+rm -f system/system/app/VoiceAssist/VoiceAssist.apk.* 2>/dev/null
+cat system/system/priv-app/Browser/Browser.apk.* 2>/dev/null >> system/system/priv-app/Browser/Browser.apk
+rm -f system/system/priv-app/Browser/Browser.apk.* 2>/dev/null
+cat system/system/priv-app/MiuiVideo/MiuiVideo.apk.* 2>/dev/null >> system/system/priv-app/MiuiVideo/MiuiVideo.apk
+rm -f system/system/priv-app/MiuiVideo/MiuiVideo.apk.* 2>/dev/null
+cat system/system/product/app/WebViewGoogle/WebViewGoogle.apk.* 2>/dev/null >> system/system/product/app/WebViewGoogle/WebViewGoogle.apk
+rm -f system/system/product/app/WebViewGoogle/WebViewGoogle.apk.* 2>/dev/null
+cat vendor/data-app/SmartHome/SmartHome.apk.* 2>/dev/null >> vendor/data-app/SmartHome/SmartHome.apk
+rm -f vendor/data-app/SmartHome/SmartHome.apk.* 2>/dev/null
